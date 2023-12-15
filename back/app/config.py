c3ecd99839b10a698c895ab3d8feef3b6323e323
tunsmm <dragon553180@gmail.com>
2023-12-15 21:06:32 +0300

    Add base config of creating directories

---
+++ back/app/config.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+
+# Directory to store uploaded files
+UPLOAD_DIR = Path("uploads")
+UPLOAD_DIR.mkdir(exist_ok=True)
+
+# Directory to store result of generating schedule
+GENERATED_SCHEDULE_DIR = Path("generated_schedule")
+GENERATED_SCHEDULE_DIR.mkdir(exist_ok=True)
