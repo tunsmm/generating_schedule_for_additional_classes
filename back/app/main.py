d0a553bb39a09bf47d204d2c85b29e75985235af
hanahana <enigma597@mail.ru>
2023-12-16 18:54:08 +0300

    feat(client,server): add client files and disable cors for server

--- back/app/main.py
+++ back/app/main.py
@@ -4,8 +4,22 @@ from config import GENERATED_SCHEDULE_DIR, UPLOAD_DIR
 
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.post("/uploadfile/")
