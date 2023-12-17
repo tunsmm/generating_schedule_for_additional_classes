b96fed44fd33d167cdd748b4fb84d0a4d80540f2
tunsmm <dragon553180@gmail.com>
2023-12-18 00:32:45 +0300

    Add signature of generating excel based on data

---
+++ back/app/utils.py
@@ -0,0 +1,19 @@
+def generate_excel(extra_classes: list, extra_groups: list, file_path: str):
+    """
+    Generate excel based on data like that 
+
+    extra_classes:
+    [
+        {"weekday": 0, "class": 0, "groups": ["\u0421\u0418\u0418-1", "C\u0418\u0418-2"]}, 
+        {"weekday": 0, "class": 1, "groups": ["\u0421\u0418\u0418-3"]}
+    ]
+
+    extra_groups:
+    [
+        {"name": "\u0421\u0418\u0418-1", "students": [
+            {"group": "\u042d\u041f-362", "name": "6538ec43a04fb531ff864a26"}, 
+            {"group": "\u042d\u041f-362", "name": "7832ac43a04fb545ff864a21"}]
+        }
+    ]
+    """
+    pass
