f307b3a354274ce7b8755c0bd5b6e0e8997eafb0
tunsmm <dragon553180@gmail.com>
2023-12-18 12:39:54 +0300

    Change getting name of schedule file

--- back/app/schedule.py
+++ back/app/schedule.py
@@ -1,4 +1,4 @@
-from config import GENERATED_SCHEDULE_DIR, UPLOAD_DIR, WEEKDAYS_COUNT, CLASSES_COUNT, MAX_GROUP_CAPACITY
+from config import GENERATED_FILE_PATH, UPLOAD_DIR, WEEKDAYS_COUNT, CLASSES_COUNT, MAX_GROUP_CAPACITY
 
 from alg.factories.timetablefactory import create_timetable_alg
 from parser.mainparser import MainParser
@@ -13,27 +13,25 @@ def get_generated_schedule_name():
 
 
 def __generate_schedule() -> str:
-    # main_parser = MainParser()
+    main_parser = MainParser()
 
-    # file_path = UPLOAD_DIR / "studentsGroups" / "Iskhodny_spisok_xlsx.csv"
-    # parsed_extra_classes_data = main_parser.getExtraClassesJson(file_path)
-    # parsed_groups_students_data = main_parser.defGroupsStudentsJson(file_path)
+    file_path = UPLOAD_DIR / "studentsGroups" / "Iskhodny_spisok_xlsx.csv"
+    parsed_extra_classes_data = main_parser.getExtraClassesJson(file_path)
+    parsed_groups_students_data = main_parser.defGroupsStudentsJson(file_path)
 
-    # file_dir = UPLOAD_DIR / "schedules"
-    # parsed_groups_classes_data = main_parser.getGroupsClassesJson(file_dir)
+    file_dir = UPLOAD_DIR / "schedules"
+    parsed_groups_classes_data = main_parser.getGroupsClassesJson(file_dir)
 
-    # data_to_alg = {
-    #     "classes": parsed_groups_classes_data,
-    #     "main_groups": parsed_groups_classes_data,
-    #     "extra_classes": parsed_extra_classes_data
-    # }
+    data_to_alg = {
+        "classes": parsed_groups_classes_data,
+        "main_groups": parsed_groups_classes_data,
+        "extra_classes": parsed_extra_classes_data
+    }
 
-    # extra_groups, extra_classes = time_table_alg.process(data_to_alg)
+    extra_groups, extra_classes = time_table_alg.process(data_to_alg)
 
-    # filename = 'result_schedule.xlsx'
-    file_path = GENERATED_SCHEDULE_DIR / 'result.xlsx'
+    file_path = GENERATED_FILE_PATH
 
-    # TODO: Add generating excel
-    # generate_excel(extra_classes, extra_groups, file_path)
+    generate_excel(extra_classes, extra_groups, file_path)
 
     return file_path
