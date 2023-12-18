18bfc9a3ee7a97614ad7a771e59cc2bee680c1f8
tunsmm <dragon553180@gmail.com>
2023-12-18 09:56:58 +0300

    Add calling alg part

--- back/app/schedule.py
+++ back/app/schedule.py
@@ -1,5 +1,6 @@
 from config import GENERATED_SCHEDULE_DIR, UPLOAD_DIR, WEEKDAYS_COUNT, CLASSES_COUNT, MAX_GROUP_CAPACITY
 
+from alg.factories.timetablefactory import create_timetable_alg
 from parser.mainparser import MainParser
 from utils import generate_excel
 
@@ -14,7 +15,7 @@ def get_generated_schedule_name():
 def __generate_schedule() -> str:
     main_parser = MainParser()
 
-    file_path = UPLOAD_DIR / "studentsGroups" / "Исходный список.xlsx"
+    file_path = UPLOAD_DIR / "studentsGroups" / "Iskhodny_spisok_xlsx.csv"
     parsed_extra_classes_data = main_parser.getExtraClassesJson(file_path)
     parsed_groups_students_data = main_parser.defGroupsStudentsJson(file_path)
 
@@ -27,7 +28,7 @@ def __generate_schedule() -> str:
         "extra_classes": parsed_extra_classes_data
     }
 
-    extra_groups, extra_classes = time_table_alg.process(data_to_alg)  # Here is your algorithm result
+    extra_groups, extra_classes = time_table_alg.process(data_to_alg)
 
     filename = 'result_schedule.xlsx'
     file_path = GENERATED_SCHEDULE_DIR / filename
