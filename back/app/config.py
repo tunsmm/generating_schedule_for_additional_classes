34b1bd9ab4b908b280b5498a754a19d77f1edec1
tunsmm <dragon553180@gmail.com>
2023-12-18 12:40:00 +0300

    Change getting name of schedule file

--- back/app/config.py
+++ back/app/config.py
@@ -9,6 +9,8 @@ Path("uploads/studentsGroups").mkdir(exist_ok=True)
 # Directory to store result of generating schedule
 GENERATED_SCHEDULE_DIR = Path("generated_schedule")
 GENERATED_SCHEDULE_DIR.mkdir(exist_ok=True)
+GENERATED_FILE_NAME = 'result.xlsx'
+GENERATED_FILE_PATH = GENERATED_SCHEDULE_DIR / GENERATED_FILE_NAME
 
 WEEKDAYS_COUNT = 6
 CLASSES_COUNT = 6
